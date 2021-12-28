d05973686b98a37ddc4442da6e6c8f4c58f637c0
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-28 16:36:50 +0200

    Add functioning navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-function App() {
-  return (
-    <h1>Welcome to my portfolio</h1>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+import HomePage from './pages/home';
+import AboutPage from './pages/about';
+import ContactPage from './pages/contact';
+import ProjectsPage from './pages/projects';
+
+const App = () => (
+  <Router>
+    <NavBar />
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/projectsPage" element={<ProjectsPage />} />
+      <Route path="/aboutPage" element={<AboutPage />} />
+      <Route path="/contactPage" element={<ContactPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
