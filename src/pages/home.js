2a49bb0568c3bb72d4df2a280e7de72c93ddc236
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-28 22:43:50 +0200

    Add home page and minimal styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,56 @@
+import React from 'react';
+import { SiLinkedin, SiGithub } from 'react-icons/si';
+import { FaTwitterSquare } from 'react-icons/fa';
+import { AiFillMediumCircle } from 'react-icons/ai';
+import { IoMdMail } from 'react-icons/io';
+
 const HomePage = () => (
-  <div>Home page!</div>
+  <div className="homemain">
+    <section className="intro">
+      <header>
+        <h1>Hey there!</h1>
+        <h3>I&apos;m Chawanzi Ng&apos;uni</h3>
+        <h4>Full Stack Developer</h4>
+      </header>
+      <p>
+        I can help you build a product,
+        feature, or website as well as design
+        it. Look through some of my work
+        and experience. If you like what you
+        see and have a project you need
+        coded, do not hesitate to contact me.
+      </p>
+    </section>
+    <section className="socials">
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/chawanzi-ng-uni/" target="_blank" rel="noreferrer">
+            <SiLinkedin className="icon" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/chaw-bot" target="_blank" rel="noreferrer">
+            <SiGithub className="icon_second" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/chawfronaut" target="_blank" rel="noreferrer">
+            <FaTwitterSquare className="icon" />
+          </a>
+        </li>
+        <li>
+          <a href="https://medium.com/@afrononymous" target="_blank" rel="noreferrer">
+            <AiFillMediumCircle className="icon_second" />
+          </a>
+        </li>
+        <li>
+          <a href="add link to contact page">
+            <IoMdMail className="icon" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
 );
 
 export default HomePage;
