da6d33cbf1f31890eaecf0b67515178ad04a36e8
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-17 00:54:31 +0200

    Style mobile project page

--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -2,35 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProjectsPage = ({ projects }) => (
-  <div className="projectsmain">
+  <div className="projectsmain p-3">
     <header>
-      <h3>
+      <h3 className="font-colour">
         Here are some of the projects I&apos;ve created and collaborated on..
       </h3>
     </header>
-    <section>
+    <section className="">
       {
           projects.map((project) => (
-            <div key={project.name} className="projects">
+            <div key={project.name} className="card mt-3 p-3 projects">
               <div>
-                <h1>{project.name}</h1>
+                <h5 className="">{project.name}</h5>
                 <img src={project.image} alt={project.name} className="images" />
               </div>
               <div>
-                <p>{project.description}</p>
-                <a href={project.live_demo}>
-                  <input type="submit" value="Live Demo" />
-                </a>
-                <a href={project.source_code}>
-                  <input type="submit" value="Source Code" />
-                </a>
-                <ul>
+                <ul className="d-flex justify-content-evenly mt-3 lang">
                   {
                     project.languages.map((language) => (
-                      <li key={language}>{language}</li>
+                      <li key={language} className="p-2">{language}</li>
                     ))
                   }
                 </ul>
+                <p className="mt-3">{project.description}</p>
+                <div className="d-flex justify-content-evenly">
+                  <a href={project.live_demo}>
+                    <input type="submit" value="Live Demo" className="p-2 link-btn text-light" />
+                  </a>
+                  <a href={project.source_code}>
+                    <input type="submit" value="Source Code" className="p-2 link-btn text-light" />
+                  </a>
+                </div>
               </div>
             </div>
           ))
