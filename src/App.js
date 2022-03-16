99dde30c8376195ff0ec1e115f99074bb7f47b2a
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-16 11:14:52 +0200

    Add project screenshots

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import NavBar from './components/Navbar';
 import HomePage from './pages/home';
 import AboutPage from './pages/about';
 import ContactPage from './pages/contact';
-import ProjectsPage from './pages/projects';
+import ProjectsPage from './pages/projectsPage';
+import projects from './helpers/projectList';
 
 const App = () => (
   <Router>
     <NavBar />
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-      <Route path="/projectsPage" element={<ProjectsPage />} />
+      <Route path="/projectsPage" element={<ProjectsPage projects={projects} />} />
       <Route path="/aboutPage" element={<AboutPage />} />
       <Route path="/contactPage" element={<ContactPage />} />
     </Routes>
